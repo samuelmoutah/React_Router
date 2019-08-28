cce33f407f199f4ef2ef96b076cbb6a1df8d897f
samuelmo <samuelmo78@gmail.com>
2019-08-28 02:18:29 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom'
+
+import Home from './Home'
+import About from './About'
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  </BrowserRouter>
+  
+);
+
+export default App;
